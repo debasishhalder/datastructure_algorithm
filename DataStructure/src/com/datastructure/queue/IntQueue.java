a81e86340000ba2960ced6ce9ee9658016564451
Debasish <debasishhalder2010@gmail.com>
2019-07-02 21:24:04 +0530

    added class for queue implementation

---
+++ DataStructure/src/com/datastructure/queue/IntQueue.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.datastructure.queue;
+
+/**
+ * @author Debasish
+ *
+ */
+public class IntQueue {
+
+}
---
+++ DataStructure/src/com/datastructure/queue/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.datastructure.queue;
+
+/**
+ * @author Debasish
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
